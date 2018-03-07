64ea254a3e53b7495b88ae829448a78d577804bb
cayman01 <cayman01.filiales@gmail.com>
2018-03-07 17:18:52 -0500

    Until master page & remember

--- Lands/Lands/ViewModels/MenuItemViewModel.cs
+++ Lands/Lands/ViewModels/MenuItemViewModel.cs
@@ -34,7 +34,7 @@
                 var mainViewModel = MainViewModel.GetInstance();
                 mainViewModel.Token = string.Empty;
                 mainViewModel.TokenType = string.Empty;
-                Application.Current.MainPage = new LoginPage();
+                Application.Current.MainPage = new NavigationPage(new LoginPage());
             }
         }
         #endregion
