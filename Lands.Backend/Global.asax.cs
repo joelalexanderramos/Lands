f711974cc3535cb40b443150af8b9b5e0e4cc2c6
Cayman01 <cayman01.filiales@gmail.com>
2018-03-07 19:07:41 -0500

    User types

--- Lands.Backend/Global.asax.cs
+++ Lands.Backend/Global.asax.cs
@@ -1,14 +1,19 @@
 ﻿namespace Lands.Backend
 {
+    using System.Data.Entity;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
     using Helpers;
+    using Models;
 
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<LocalDataContext, 
+                Migrations.Configuration>());
             this.CheckRolesAndSuperUser();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
