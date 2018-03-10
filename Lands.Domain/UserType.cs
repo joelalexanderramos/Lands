362421f0d2fe2324b4f326dd95ef647b4332cca8
Cayman01 <cayman01.filiales@gmail.com>
2018-03-09 20:33:55 -0500

    Add user type

---
+++ Lands.Domain/UserType.cs
@@ -0,0 +1,17 @@
+﻿namespace Lands.Domain
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class UserType
+    {
+        [Key]
+        public int UserTypeId { get; set; }
+
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
