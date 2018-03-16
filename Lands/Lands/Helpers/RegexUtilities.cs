bdd382a43eea78a4e15d602e5e584f7ab0bc9cdd
Joel Ramos <joelalexanderramos@gmail.com>
2018-03-16 11:40:21 -0400

    Register Page

---
+++ Lands/Lands/Helpers/RegexUtilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Helpers
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public static class RegexUtilities
+    {
+        public static bool IsValidEmail(string email)
+        {
+            var expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(email, expresion))
+            {
+                if (Regex.Replace(email, expresion, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
