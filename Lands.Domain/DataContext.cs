48d76ab788ac91cb3a2cad6891b7d77736c04285
Joel Ramos <joelalexanderramos@gmail.com>
2018-04-27 16:55:47 -0400

    Part 050 (Team Controller)

--- Lands.Domain/DataContext.cs
+++ Lands.Domain/DataContext.cs
@@ -8,8 +8,10 @@
         {
         }
 
-        public System.Data.Entity.DbSet<Lands.Domain.User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
 
-        public System.Data.Entity.DbSet<Lands.Domain.UserType> UserTypes { get; set; }
+        public DbSet<UserType> UserTypes { get; set; }
+
+        public DbSet<Team> Teams { get; set; }
     }
 }
---
+++ Lands.Domain/Team.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lands.Domain
+{
+    public class Team
+    {
+        [Key]
+        public int TeamId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [MaxLength(50, ErrorMessage = "The field {0} only can contains a maximum of {1} characters lenght.")]
+        [Index("Team_Name_Index", IsUnique =true)]
+        public string Name { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImagePath { get; set; }
+    }
+}
